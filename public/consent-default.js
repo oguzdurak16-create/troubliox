05fe078a2612869b034f7ec48a2f52766e15a18b
oguzdurak16-create <oguzdurak16@gmail.com>
2026-07-27 23:06:44 +0300

    Add blocking Consent Mode bootstrap

---
+++ public/consent-default.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+window.gtag = window.gtag || function gtag(){ window.dataLayer.push(arguments); };
+window.gtag('consent', 'default', {
+  analytics_storage: 'denied',
+  ad_storage: 'denied',
+  ad_user_data: 'denied',
+  ad_personalization: 'denied',
+  functionality_storage: 'granted',
+  security_storage: 'granted',
+  wait_for_update: 500
+});
+window.gtag('set', 'ads_data_redaction', true);
+window.gtag('set', 'url_passthrough', true);
