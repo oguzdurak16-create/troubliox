6d4e6834ba72a4ba27439a3f474c5f6cd11a69de
oguzdurak16-create <oguzdurak16@gmail.com>
2026-08-23 22:33:44 +0300

    fix Troublio analytics consent-mode measurement

--- src/components/ConsentManager.tsx
+++ src/components/ConsentManager.tsx
@@ -95,7 +95,7 @@ export function ConsentManager({ gaId }: { gaId?: string }) {
 
   return (
     <>
-      {ready && preferences.analytics && gaId ? (
+      {gaId ? (
         <>
           <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
           <Script id="ga4" strategy="afterInteractive">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}window.gtag=window.gtag||gtag;gtag('js',new Date());gtag('config','${gaId}',{anonymize_ip:true});`}</Script>
